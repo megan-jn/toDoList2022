dae3eb0d8ea649c2d2aedda2420b091b18e3705c
Megan Nguy <51968284+megan-jn@users.noreply.github.com>
2022-07-15 16:25:31 -0700

    did static to-dos (iteration 0)

---
+++ toDoList2022/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  toDoList2022
+//
+//  Created by Megan on 7/15/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name = "OH NOO_OO"
+    var important = true
+}
---
+++ toDoList2022/ToDoTableViewController.swift
@@ -0,0 +1,106 @@
+//
+//  ToDoTableViewController.swift
+//  toDoList2022
+//
+//  Created by Megan on 7/15/22.
+//
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+
+    var toDos : [ToDo] = [] // creates empty array of the class that we made
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        toDos = createToDos()
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    } // viewDidLoad ends
+    
+    func createToDos() -> [ToDo]{
+        let sleep = ToDo()
+        sleep.name = "sleep by 12AM"
+        sleep.important = false
+        
+        let packBags = ToDo()
+        packBags.name = "pack dirty clothes"
+        
+        return [sleep,packBags]
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1 // not in ToDoList directions!!!
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return toDos.count // makes number of rows = number of items in array
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        let toDo = toDos[indexPath.row]
+        
+        if toDo.important{
+            cell.textLabel?.text = "😡" + toDo.name
+        } else {
+            cell.textLabel?.text = toDo.name
+        }
+        return cell
+    
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
